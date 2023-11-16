aec20244642eea0cabb7a02b6c8581d69b8be8b8
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2023-11-16 16:27:35 -0400

    Versao 2.0

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import userController from '../controllers/userController.js';
 import verifyPermissions from '../controllers/verifyPermissions.js';
+import verifyToken from '../controllers/verifyToken.js';
 const userRouter = express.Router();
 
 userRouter.post('/register', userController.register);
 userRouter.post('/login', userController.login);
+userRouter.put('/reset',verifyToken,userController.resetPassword);
 userRouter.get('/all',verifyPermissions,userController.getAllUsers);
 
 export default userRouter;
